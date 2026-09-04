83a5ce19c58ce70682175192e27db5e17947b51d
Eduardo <dudumadke851@gmail.com>
2026-09-04 20:15:19 -0300

    Classe aerorporto, fui tentar ser mais inteligente que a prof e tomei gap

---
+++ aeroporto.java
@@ -0,0 +1,35 @@
+public class aeroporto {
+
+	public void cadastroVoo() {
+
+	}
+
+	public void procuraVoo() {
+
+	}
+
+	public void listaVoo() {
+
+	}
+
+	public void listaVooAutorizado() {
+
+	}
+
+	public void listaVooPendentes() {
+
+	}
+
+	public String motivoPendencia() {
+		return null;
+	}
+
+	public void vooMaiorCusto() {
+
+	}
+
+	public void qntVooCategoria() {
+
+	}
+
+}
